9e6f00eae68ae2cfc1f552bab788c130d6d54db2
rrchampavat <work.rrchampavat@gmail.com>
2023-08-25 16:43:05 +0530

    INTG: Mantine and tailwindcss integration completed

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3d403a",
+        secondary: "#0c0c0d",
+        accent: "#736d78",
+        text: "#f5f5f4",
+        background: "#0d0d0c"
+      }
+    }
+  },
+  plugins: []
+};
