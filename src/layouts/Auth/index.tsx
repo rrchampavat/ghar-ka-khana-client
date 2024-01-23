9af66054d1d65f793e982d1367d8396c67779ef9
RutvikrajWork <work.rrchampavat@gmail.com>
2024-01-24 02:24:03 +0530

    FIX: Fixed dark mode bug

--- src/layouts/Auth/index.tsx
+++ src/layouts/Auth/index.tsx
@@ -1,34 +1,15 @@
 import { Navigate, Outlet } from "react-router-dom";
 import Cookies from "js-cookie";
-import useDarkMode from "use-dark-mode";
-import { useEffect } from "react";
 
 const AuthLayout = () => {
   const isAuthenticated = !!Cookies.get("accessToken");
-  const { value: isDarkMode } = useDarkMode(false);
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document
-        .getElementsByTagName("div")[1]
-        ?.classList.add("bg-background", "dark");
-
-      return;
-    }
-
-    document.getElementsByTagName("div")[1]?.classList.remove("dark");
-  }, [isDarkMode]);
 
   if (isAuthenticated) {
     return <Navigate to="/" replace />;
   }
 
   return (
-    <main
-      className={`${
-        isDarkMode ? "dark" : ""
-      } container grid h-screen items-center bg-background text-foreground md:grid-cols-[2fr_2fr] lg:grid-cols-[3fr_2fr]`}
-    >
+    <main className="container grid h-screen items-center md:grid-cols-[2fr_2fr] lg:grid-cols-[3fr_2fr]">
       <Outlet />
     </main>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,23 @@ import "./index.css";
 import App from "./App";
 import { NextUIProvider } from "@nextui-org/react";
 import { Toaster } from "./ui/components/toast/toaster";
+import useDarkMode from "use-dark-mode";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <NextUIProvider>
-    <Toaster />
-    <App />
-  </NextUIProvider>
-  // </React.StrictMode>
-);
+const RootComponent = () => {
+  const { value: isDarkMode } = useDarkMode(false);
+
+  return (
+    // <React.StrictMode>
+    <NextUIProvider>
+      <main
+        className={`${isDarkMode ? "dark" : ""} bg-background text-foreground`}
+      >
+        <Toaster />
+        <App />
+      </main>
+    </NextUIProvider>
+    // </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<RootComponent />);
